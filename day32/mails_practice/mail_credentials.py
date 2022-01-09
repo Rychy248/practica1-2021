369df950f4fe92a7eaa6952b92c08a82a172c0f9
Rychy248 <jorgeajrhh@gmai.com>
2022-01-09 10:43:35 -0600

    Day 41 and 42 finished

---
+++ day32/mails_practice/mail_credentials.py
@@ -0,0 +1,4 @@
+MAIL="brayanexploited100@gmail.com"
+PASSWORD="brayan467"
+HOST="smtp.gmail.com"
+PORT="587"
---
+++ day33/API_ISS_overhead/mail_credentials.py
@@ -0,0 +1,12 @@
+MAIL="brayanexploited100@gmail.com"
+PASSWORD="brayan467"
+HOST="smtp.gmail.com"
+PORT="587"
+
+MAILS_TO_SEND = [
+    "jorgeajrha@gmail.com",
+    "jorgeajrhh@gmail.com",
+    "hernandezjl369@gmail.com",
+    "adelsohernandez99@gmail.com",
+    "rosalinahernandez07@gmail.com",
+]
