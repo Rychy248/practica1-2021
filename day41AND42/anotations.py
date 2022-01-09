07235b6d7412e55261306c8e6ac391e7f5120019
Rychy248 <jorgeajrhh@gmai.com>
2022-01-09 11:00:11 -0600

    fixing

---
+++ day41AND42/anotations.py
@@ -0,0 +1,53 @@
+# Submarine Cable Map
+# https://www.submarinecablemap.com/
+# The Submarine Cable Map is a free and regularly updated resource from TeleGeography.
+
+# ------------------------------------
+# <h1>–<h6>: The HTML Section Heading elements
+# https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Heading_Elements
+
+# The Adventures of Sherlock Holmes by Arthur Conan Doyle
+# https://www.gutenberg.org/ebooks/1661
+
+# HTML Headings
+# https://www.w3schools.com/html/html_headings.asp
+
+# DevDocs - DevDocs combines multiple API documentations in a fast, organized, and searchable interface.
+# https://devdocs.io/
+
+# ------------------------------------
+
+# CODEPEN
+# https://codepen.io/
+# CodePen is a social development environment for front-end designers and developers. Build and deploy a website, show off your work, build test cases to learn and debug, and find inspiration.
+
+# Thomas H. Cormen Emeritus Profesor | Departament of computer Science
+# https://www.cs.dartmouth.edu/~thc/
+
+# Jon Kleinberg 
+# http://www.cs.cornell.edu/home/kleinber/
+
+# Donald E. Knuth 
+# https://www-cs-faculty.stanford.edu/~knuth/
+
+# ------------------------------------
+
+# List of UTF-8 Characteres
+# https://www.fileformat.info/info/charset/UTF-8/list.htm
+
+# List of Unicode Characters 
+# https://unicode-table.com/en/
+
+# Joel Spolsky on Unicode
+# https://www.joelonsoftware.com/2003/10/08/the-absolute-minimum-every-software-developer-absolutely-positively-must-know-about-unicode-and-character-sets-no-excuses/
+
+# Emmet Cheat Sheet
+# https://docs.emmet.io/cheat-sheet/
+
+# ------------------------------------
+# HTML emphasis vs italicises tags
+# https://developer.mozilla.org/en-US/docs/Web/HTML/Element/em#%3Ci%3E_vs._%3Cem%3E
+
+# ------------------------------------
+# The WikiGame
+# https://www.thewikigame.com/
